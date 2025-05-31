8432e65355028d528656dd78b4e5ad79dedb714d
illusion <1161524405@qq.com>
2025-05-31 22:27:45 +0800

    add check member if exits

--- core/src/application/member/repositories/member_repository.rs
+++ core/src/application/member/repositories/member_repository.rs
@@ -8,4 +8,5 @@ pub trait MemberRepository {
     async fn update_member(&self, member: Member);
     async fn get_member_by_id(&self, id: u64) -> Option<Member>;
     async fn get_member_by_name(&self, first_name: String, last_name: String) -> Option<Member>;
+    async fn check_member_if_exists(&self, id: u64) -> bool;
 }
