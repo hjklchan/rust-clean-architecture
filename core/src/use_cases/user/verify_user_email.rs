1e9967a24375b81eb942fb3e9b6c806fc70626a5
hjkl1 <1161524405@qq.com>
2025-06-05 18:41:55 +0800

    add unused marker

--- core/src/use_cases/user/verify_user_email.rs
+++ core/src/use_cases/user/verify_user_email.rs
@@ -1,10 +1,12 @@
 use crate::use_cases::interfaces::validators::email_validator::EmailValidator;
 
 
+#[allow(unused)]
 pub struct VerifyUserEmail<V: EmailValidator> {
     email_validator: V,
 }
 
+#[allow(unused)]
 impl<V: EmailValidator> VerifyUserEmail<V> {
     fn execute(&self, email: String) -> bool {
         self.email_validator.is_valid_email(&email)
