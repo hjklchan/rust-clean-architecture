b440d7be3131e6f0b841f0cd4060efe9a78da446
hjkl1 <1161524405@qq.com>
2025-06-14 16:22:39 +0800

    add functions in the user domain

--- domain/src/v2/user/user.rs
+++ domain/src/v2/user/user.rs
@@ -4,12 +4,29 @@ use crate::v2::user::{enums::user_status::UserStatus, value_objects::user_id::Us
 
 // Aggregation root for user entity.
 pub struct User {
-    id: UserId,
-    username: String,
-    email: String,
-    status: UserStatus,
-    password: String,
-    created_at: DateTime<Utc>,
-    updated_at: DateTime<Utc>,
-    deleted_at: Option<DateTime<Utc>>,
+    pub id: UserId,
+    pub username: String,
+    pub email: String,
+    pub status: UserStatus,
+    pub password: String,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+    pub deleted_at: Option<DateTime<Utc>>,
+}
+
+impl User {
+    pub fn new(id: UserId, username: String, email: String, password: String) -> Self {
+        let utc_now = Utc::now();
+
+        Self {
+            id,
+            username,
+            email,
+            status: Default::default(),
+            password,
+            created_at: utc_now,
+            updated_at: utc_now,
+            deleted_at: None,
+        }
+    }
 }
