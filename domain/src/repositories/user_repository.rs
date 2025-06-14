71162ebf7c2ed67d44b865dfb4246bd5132ee8b3
hjkl1 <1161524405@qq.com>
2025-06-14 16:16:43 +0800

    add method get_by_email in the user repo

--- domain/src/repositories/user_repository.rs
+++ domain/src/repositories/user_repository.rs
@@ -1,11 +1,13 @@
 use std::error::Error;
+use uuid::Uuid;
 
-use crate::entities::user::User;
+use crate::v2::user::User;
 
 #[async_trait::async_trait]
 pub trait UserRepository: Send + Sync {
-    async fn save(&self, user: User) -> Result<u64, UserRepositoryError>;
+    async fn save(&self, user: User) -> Result<Uuid, UserRepositoryError>;
     async fn get_by_id(&self, id: u64) -> Result<Option<User>, UserRepositoryError>;
+    async fn get_by_email(&self, email: &str) -> Result<Option<User>, UserRepositoryError>;
     async fn soft_delete(&self, id: u64) -> Result<(), UserRepositoryError>;
 }
 
