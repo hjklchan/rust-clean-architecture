2dc64d4b9fa68668c4e66986f29763387848f394
hjkl1 <1161524405@qq.com>
2025-06-14 16:23:08 +0800

    add some functions in the user_id value object

--- domain/src/v2/user/value_objects/user_id.rs
+++ domain/src/v2/user/value_objects/user_id.rs
@@ -8,10 +8,20 @@ impl UserId {
     pub fn generate() -> Self {
         Self(Uuid::new_v4())
     }
+
+    pub fn get_id(&self) -> Uuid {
+        self.0
+    }
 }
 
 impl Display for UserId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0)
+    }
+}
+
+impl From<Uuid> for UserId {
+    fn from(value: Uuid) -> Self {
+        Self(value)
     }
 }
